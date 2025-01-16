49eb7412460abb5202ac17e57fbbce06cfd17cf1
D Akshaykumar <d.akshaykumar17@gmail.com>
2025-01-17 00:07:01 +0530

    Add files via upload

---
+++ Business Problems Netflix.sql
@@ -0,0 +1,19 @@
+-- Business Problems
+
+1. Count the number of Movies vs TV Shows
+2. Find the most common rating for movies and TV shows
+3. List all movies released in a specific year (e.g., 2020)
+4. Find the top 5 countries with the most content on Netflix
+5. Identify the longest movie
+6. Find content added in the last 5 years
+7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+8. List all movies that are documentaries
+9. Find all content without a director
+10. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+
+
+
+
+
+
+
